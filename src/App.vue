5f6beed08679e0c7d1d7c1fe72d2d371613d38ab
Barney Gammond <barney.gammond@gmail.com>
2023-10-09 21:21:23 +0100

    build: basic functionality

--- src/App.vue
+++ src/App.vue
@@ -2,8 +2,12 @@
   <div id="app">
       <div>
         <h3>Hello world</h3>
-        <p>{{message}}</p>
-        <button v-on:click="change_text">change</button>
+        <input
+          type="number"
+          v-model="donationAmount"
+        >
+        <p>{{ exchangedAmount }}</p>
+        <button v-on:click="fetchExchange">Calculate</button>
       </div>
   </div>
 </template>
@@ -13,12 +17,17 @@ export default {
   name: 'App',
   data() {
     return {
-      message: "button is not clicked"
+      donationAmount: "",
+      exchangedAmount: "",
     }
   },
   methods: {
-    change_text() {
-      this.message = "button was clicked"
+    async fetchExchange () {
+      const response = await fetch('https://founderspledge.github.io/fp-interview-exercise-api/currency.json')
+      const { quotes } = await response.json()
+      // TODO Contingency in case quotes.USDGBP is not available
+      this.exchangedAmount = (this.donationAmount * quotes.USDGBP).toFixed(2)
+      console.log(quotes)
     }
   }
 }
